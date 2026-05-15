92091543d178242519b323719eb2c77a2eed037b
HH <1309969814@qq.com>
2026-05-15 23:33:55 +0800

    fix:creation解析

--- src/main/java/violet/trending/flink/common/pojo/Creation.java
+++ src/main/java/violet/trending/flink/common/pojo/Creation.java
@@ -1,11 +1,20 @@
 package violet.trending.flink.common.pojo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Creation {
-    private Long creationId;
-    private Long userId;
-    private String category;
-    private Integer status;
+    private Payload payload;
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Payload {
+        private Long id;
+        private Long creationId;
+        private Long userId;
+        private String category;
+        private Integer status;
+    }
 }
